bcbe9065be09b3e1a9ad2fe6ff58f862256c8bf9
MarinaZRocha <marina.zr.flora@gmail.com>
2024-11-26 10:31:00 -0300

    integrated fake blink and jaw; added stop face code

--- src/fake_jaw/fake_jaw.ino
+++ src/fake_jaw/fake_jaw.ino
@@ -2,6 +2,13 @@
 
 Servo JAW_RIGHT;
 Servo JAW_LEFT;
+Servo EYELID_RIGHT;
+Servo EYELID_LEFT;
+
+int close_L = 100;
+int open_L = 20;
+int close_R = 100;
+int open_R = 0;
 
 int close_L = 97;
 int open_L = 125;
@@ -20,6 +27,21 @@ int open_R = 80;
 
     delay(400);
   }
+  void blink(){
+
+    delay(1500);
+
+    EYELID_RIGHT.write(close_R);
+    EYELID_LEFT.write(close_L);
+
+    delay(200);
+    
+    EYELID_RIGHT.write(open_R);
+    EYELID_LEFT.write(open_L);
+
+
+  }
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,6 +52,7 @@ void setup() {
 
 void loop() {
 
+  fake_jaw();
   blink();
      
 }
---
+++ src/stop_face/stop_face.ino
@@ -0,0 +1,46 @@
+#include <Servo.h> 
+
+
+Servo EYEBROW_HEIGHT_RIGHT;
+Servo EYEBROW_HEIGHT_LEFT;
+Servo EYEBROW_ANGLE_RIGHT;
+Servo EYEBROW_ANGLE_LEFT;
+Servo EYELID_RIGHT;
+Servo EYELID_LEFT;
+Servo EYE_HORIZONTAL;
+Servo EYE_VERTICAL;
+Servo JAW_LEFT;
+Servo JAW_RIGHT;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  EYEBROW_HEIGHT_RIGHT.attach(4);
+  EYEBROW_HEIGHT_LEFT.attach(3);
+  EYEBROW_ANGLE_RIGHT.attach(5);
+  EYEBROW_ANGLE_LEFT.attach(6);
+  EYELID_RIGHT.attach(8);
+  EYELID_LEFT.attach(10);
+  EYE_HORIZONTAL.attach(2);
+  EYE_VERTICAL.attach(7);
+  JAW_LEFT.attach(11);
+  JAW_RIGHT.attach(12); 
+
+  EYEBROW_HEIGHT_RIGHT.write(70);
+  EYEBROW_HEIGHT_LEFT.write(70);
+  EYEBROW_ANGLE_RIGHT.write(130);
+  EYEBROW_ANGLE_LEFT.write(150);
+  EYELID_RIGHT.write(10);
+  EYELID_LEFT.write(20);
+  EYE_HORIZONTAL.write(60);
+  EYE_VERTICAL.write(102);
+  JAW_LEFT.write(125);
+  JAW_RIGHT.write(80);
+}
+
+void loop() {
+     
+}
